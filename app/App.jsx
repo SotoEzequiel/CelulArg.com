90cb3a3fb6bb0edbcb19522b6234f60f936f965d
sotoezequiel <ezequiel_soto_f4@hotmail.com>
2023-04-25 18:54:05 -0300

    ad

--- app/App.jsx
+++ app/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import {StyleSheet, Text, View} from "react-native";
 import Home from "./components/Home/Home";
+import {TailwindProvider} from 'tailwind-rn';
+import utilities from './tailwind.json';
 
 const App = () => {
   return(
-    <Home/>
+    <TailwindProvider utilities={utilities}>
+      <Home/>
+    </TailwindProvider>
   ) 
 }
 
---
+++ app/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { withExpoSnack } from 'nativewind';
+import {useTailwind} from 'tailwind-rn';
+import { Text, View } from 'react-native';
+import { styled } from 'nativewind';
+
+
+
+
+const Home = () => {
+
+  const tailwind = useTailwind();
+    return (
+        <Text style={tailwind("text-4xl text-red-600")}>Hola Mundo</Text>
+    )
+}
+
+export default Home;
